28a89cfa88d1b29e44e5b72becf8f94c02166256
Hugo Duksis <duksis@gmail.com>
2017-07-21 11:56:13 +0200

    Use System.get_env for poolsize as we have to convert it to integer

--- config/prod.exs
+++ config/prod.exs
@@ -64,5 +64,5 @@ config :logger, level: :info
 config :match_or_not, MatchOrNot.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: {:system, "DATABASE_URL"},
-  pool_size: String.to_integer({:system, "POOL_SIZE"} || "10"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
   ssl: true

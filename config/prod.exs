8abc6922de101f08b7fe4a57a4b4295e86dd7e7c
Hugo Duksis <duksis@gmail.com>
2017-07-21 11:53:17 +0200

    configure app for production heroku usage

--- config/prod.exs
+++ config/prod.exs
@@ -13,8 +13,10 @@ use Mix.Config
 # which you typically run after static files are built.
 config :match_or_not, MatchOrNot.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
+  url: [scheme: "https", host: "my-elixir-test-app.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: {:system, "SECRET_KEY_BASE"}
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -58,4 +60,9 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs
 # which should be versioned separately.
-import_config "prod.secret.exs"
+
+config :match_or_not, MatchOrNot.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: {:system, "DATABASE_URL"},
+  pool_size: String.to_integer({:system, "POOL_SIZE"} || "10"),
+  ssl: true
